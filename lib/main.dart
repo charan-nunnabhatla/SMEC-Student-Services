435a49fe1d71ef0388611138931fca4b914b3413
Charan <charan.reings125@gmail.com>
2023-11-02 19:48:42 +0530

    dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:stmarys/pages/services.dart';
 import 'firebase_options.dart';
-import 'package:stmarys/pages/homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,7 +23,7 @@ class StMarysState extends State<StMarys> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // darkTheme: ThemeData.dark(useMaterial3: true) kljdf,
+      darkTheme: ThemeData.dark(useMaterial3: true) ,
       debugShowCheckedModeBanner: false,
       home: StudentServices(),
     );
